aac1bfd0d9f0fd7163787fa657018c8f00473d1c
yasmeen <yasmeenhany14@gmail.com>
2023-09-24 16:26:02 +0300

    added path properly

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 # config file to set up client SSH configuration so that we can connect without typing a password
-file { '~/.ssh/config':
+file { '/home/ubuntu/.ssh/config':
     ensure  => 'file',
     owner   => 'ubuntu',
     group   => 'ubuntu',
     mode    => '0600',
     content => "
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
+    Host *
+        IdentityFile /home/ubuntu/.ssh/school
+        PasswordAuthentication no
     ",
 }
