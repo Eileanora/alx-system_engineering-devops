d028d17e2d5f00270d537df3e0b8613c85e6c3ca
yasmeen <yasmeenhany14@gmail.com>
2023-09-24 17:27:28 +0300

    el r7ma mn 3ndk yarb

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,8 @@
 # config file to set up client SSH configuration so that we can connect without typing a password
 file { '~/.ssh/config':
     ensure  => 'file',
-    path    => 'etc/ssh/ssh_config',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0600',
+    path    => '/etc/ssh/ssh_config',
     content => "
     IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-    ",
+    PasswordAuthentication no",
 }
