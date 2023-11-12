cfa57247c57af5398266d0a5eda20934b7ad1bee
yasmeen <yasmeenhany14@gmail.com>
2023-11-12 22:26:28 +0200

    fix puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # this script is used to fix the web server by fixing a typo in file name in file wp-setting.php
 $file = '/var/www/html/wp-settings.php'
 exec { 'replace':
-  command => "sed -i 's/phpp/php/g' $file",
+  command => "sed -i 's/phpp/php/g' ${file}",
   path    => ['/bin', '/usr/bin']
 }
